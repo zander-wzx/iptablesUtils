b47c8c912cc782f455a0677ca5673c50cdd89218
zander-wzx <zander.wzx@gmail.com>
2019-10-25 11:29:48 +0800

    Update iptables4ddns.sh

--- iptables4ddns.sh
+++ iptables4ddns.sh
@@ -103,8 +103,8 @@ done
 
 
 ## 建立新的中转规则
-iptables -t nat -A PREROUTING -p tcp --dport $localport -j DNAT --to-destination $remote:$remoteport
+#iptables -t nat -A PREROUTING -p tcp --dport $localport -j DNAT --to-destination $remote:$remoteport
 iptables -t nat -A PREROUTING -p udp --dport $localport -j DNAT --to-destination $remote:$remoteport
-iptables -t nat -A POSTROUTING -p tcp -d $remote --dport $remoteport -j SNAT --to-source $local
+#iptables -t nat -A POSTROUTING -p tcp -d $remote --dport $remoteport -j SNAT --to-source $local
 iptables -t nat -A POSTROUTING -p udp -d $remote --dport $remoteport -j SNAT --to-source $local
 
